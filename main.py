e6ef09caed8daab1d858dd44d99cac5b497a88bc
userMeatball <cameron-bradley@hotmail.com>
2021-02-08 15:43:16 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import bs4 as bs
+import os
+from datetime import date
+import urllib.request
+from urllib.request import Request, urlopen
+
+userLink = input("Link the 4chan thread: ")
+
+req = Request(userLink, headers={'User-Agent': 'Mozilla/5.0'})  #bypass 403 error
+
+source = urllib.request.urlopen(req).read()
+soup = bs.BeautifulSoup(source, 'lxml')
+
+
+#retrieves all href links in replys
+path = []
+for child in soup.find_all('a', class_="fileThumb"):
+    path.append(child.get('href'))
+
+
+#prepends "https:" to plain links
+http = "https:"
+links = [http + p for p in path]    
+print("There are " + str(len(links)) + " files.")
+
+
+#mkdir with thread title + date filename
+threadTitle = soup.find('blockquote', class_="postMessage")
+threadText = threadTitle.text
+dirName = threadText + " " + str(date.today())
+
+if not os.path.isdir(dirName):
+    print("Creating directory: ", dirName)
+    os.mkdir(dirName)
+else:
+    print("E: Directory already present: ", dirName)
+    
+
+#downloads every file in links list
+fNum = 1 
+for link in links:
+    print("Downloading file: ", str(fNum))
+    urllib.request.urlretrieve(link, './' + dirName + '/' + str(fNum))
+    fNum += 1
+
